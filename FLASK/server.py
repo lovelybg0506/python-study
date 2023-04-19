575371623af674167548715cb61f9c9f5b29cb9d
lovelybg0506 <lovelybg0506@gmail.com>
2023-04-19 20:35:22 +0900

    read

--- FLASK/server.py
+++ FLASK/server.py
@@ -3,41 +3,51 @@ import random
 
 app = Flask(__name__)
 
-# topics = [
-#     {'id':1, 'title':'html', 'body':'html is...'},
-#     {'id':2, 'title':'css', 'body':'css is...'},
-#     {'id':3, 'title':'javascript', 'body':'javascript is...'}
-# ]
+topics = [
+    {'id':1, 'title':'html', 'body':'html is...'},
+    {'id':2, 'title':'css', 'body':'css is...'},
+    {'id':3, 'title':'javascript', 'body':'javascript is...'}
+]
 
+def template(contents, content):
+    return  f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+        </body>
+    </html>
+    '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
 
 @app.route('/')
 def index():
-    return 'hi'
-    # liTags = ''
-    # for topic in topics:
-    #     liTags = liTags + f'<li>{topic["title"]}</li>'
-    # return  f'''<!doctype html>
-    # <html>
-    #     <body>
-    #         <h1><a href="/">WEB</a></h1>
-    #         <ol>
-    #             {liTags}
-    #         </ol>
-    #         <h2>Welcome</h2>
-    #         Hello, Web
-    #     </body>
-    # </html>
-    # '''
+    return  template(getContents(), '<h2>Welcome</h2>Hello, WEB')
 
 @app.route('/create/')
 def create():
     return 'Create'
 
-@app.route('/read/<ab>')
-def read(ab):
-    print(ab)
-    return 'Read '+ab
+@app.route('/read/<int:id>/')
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    print(title,body)
+    return  template(getContents(), f'<h2>{title}</h2>{body}')
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(port=5001, debug=True)
 
