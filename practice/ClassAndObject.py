576cca0b1ad47862ad448a2eea0b17b1bce5f7be
lovelybg0506 <lovelybg0506@gmail.com>
2023-05-07 19:45:37 +0900

    ClassAndObject

--- practice/ClassAndObject.py
+++ practice/ClassAndObject.py
@@ -1,31 +1,27 @@
-# 파일 대량 rename, 분류 , 딥러닝 머신러닝, 데이터분석, 웹서버 , 뭘 만들든 많이쓰임
+# nunu = {
+#     'q' : 'eat',
+#     'w' : 'snowball' 
+# }
 
-import os # 파이썬으로 PC파일 조작
+# garen = nunu # 이렇게하면 복사가 아니고 참조(값을 공유함) 이렇게 쓰면 안됨
+# char2 = nunu # 딕셔너리형태의 데이터는 함부로 등호사용 X
+# char3 = nunu
+# char3['q'] = 'adsf'
 
-files = os.listdir('testFolder') # listdir('폴더') : 경로안에 있는 파일명 다 알려주는함수 (리스트형식)
-# print(files) # ['1.txt','2.txt','3.txt']
+# Object 생산해주는 기계 = class
+class Champion:
+    def __init__(self, q, w): # object 생성할때 def __init__ 실행됨
+        self.q = q
+        self.w = w
 
-'''
-sumtext = 'test_file_'
+    def hello(self):
+        print('안녕하세요')
 
-for i in os.listdir('testFolder'):
-    os.rename(f'testFolder/{i}', f'testFolder/{sumtext}{i}') # rename() : 파일명 변경
-'''
-
-import shutil # 파이썬으로 파일 복사
-
-for i in os.listdir('testFolder'):
-    shutil.copy(f'testFolder/{i}', f'testFolder_2/{i}') # .copy('파일경로FROM', '파일경로TO') : 파일 복사
-
-'''
-상대경로 ()
-
-절대경로 (r'C:기준 쭈우욱~~') r을 이용해서 \t 또는 \f 오류 안나도록 쓰기
-
-os.path.join('경로', '경로2') # 경로합쳐줌
-경로/경로2
-그냥 '경로' + '경로2' 로 써도됨
-
-os.getcwd() # current working directory 현재경로 알려주는 함수
-
-'''
+nunu = Champion('eat', 'snowball')
+garen = Champion('strike', 'courage')
+print(nunu.q)
+print(garen.q)
+print(nunu.w)
+print(garen.w)
+nunu.hello()
+garen.hello()
