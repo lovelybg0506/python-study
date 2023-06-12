85178a788ee82f3fd5c07f0e3e9fefee5b444735
lovelybg0506 <lovelybg0506@gmail.com>
2023-06-12 23:48:09 +0900

    2023.06.12

--- toDoList/app.py
+++ toDoList/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
 
+# Create Models
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean, default=False)
+
+# with app.app_context(): # 찾아보니 이거 추가하라는데 그래도안됨
+#     db.create_all()
 
 @app.route('/')
 def index():
